1fe96f8eab642fce722a734a37ff361075190174
Faith Cuneo <userfriendly01@gmail.com>
2019-05-23 15:52:24 -0400

    header is to the point it was before

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Header from './components/Header.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
     <Header/>,
