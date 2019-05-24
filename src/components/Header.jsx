1d99d79bef1a24343cba2590d2bbfed3b0d87adf
Faith Cuneo <userfriendly01@gmail.com>
2019-05-24 13:04:37 -0400

    Header to an initial satisfactory point to move on

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,86 +3,99 @@ import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
 import Home from '../pages/Home.jsx';
 import About from '../pages/About.jsx';
 import styled from 'styled-components';
-import {UncontrolledDropdown, DropdownToggle, DropdownItem, 
-    DropdownMenu, Navbar, Nav, NavItem, NavbarBrand} from 'reactstrap'
+import { UncontrolledDropdown, DropdownToggle, DropdownItem, DropdownMenu } from 'reactstrap'
 
 const Header = () => {
 
-    const StyledNavBar = styled(Navbar)`
-        font-size: 20px;
+    const StyledNavBar = styled.div`
         display: flex;
-        color: black;
-        
+        white-space: nowrap;
+        font-size: 16px;
+        color: #A9A9A9;
+        min-width: 90%;
+        align-items: center;
+        background-color: #E8E8E8;
     `
-    const StyledNavItem = styled(NavItem)`
-        display: block;
-        font-size: 10px;
-        padding-right: 0.5px;
-        padding-left: 0.5px;
-        font-size: 15px;
-        color: #737373;
-        &:hover {
-            color: rgba(0, 0, 0, 0.7);
-        }
-    `;
+    const NavHeader = styled.div`
+        margin-right: auto;
+        font-size: 20px;
+    `
 
-    const StyledLink = styled(Link)`
-        padding: 5px;
-        color: #16181b;
+    const StyledDropdownToggle = styled(DropdownToggle)`
+        color: #696969;
+        padding: 5px 5px;
         &:hover {
             color: #16181b;
             text-decoration: none;
         }
     `;
 
+    const StyledLink = styled(Link)`
+        display: block;    
+        padding: 5px 10px;
+        color:  #696969;
+        &:hover {
+            color: #16181b;
+            text-decoration: none;
+            
+        }
+    `;
+
+    const [isOpen1, setIsOpen1] = useState(false);
+    const [isOpen2, setIsOpen2] = useState(false);
+    const [isOpen3, setIsOpen3] = useState(false);
+
     return (
         <Router>
-            <StyledNavBar color="light" light expand="md">
-                <NavbarBrand href="/">NH Urban Sport</NavbarBrand>
-                <Nav className="ml-auto" navbar>
-                    <UncontrolledDropdown>
-                        <DropdownToggle nav caret>
-                            NH Urban Sport
-                        </DropdownToggle>
-                        <DropdownMenu>
-                            <DropdownItem>
-                                <StyledLink to="/about">About</StyledLink>
-                            </DropdownItem>
-                            <DropdownItem> Contact </DropdownItem>
-                        </DropdownMenu>
-                    </UncontrolledDropdown>
-                    <UncontrolledDropdown nav inNavbar>
-                        <DropdownToggle nav caret>
-                            Sports Leagues
-                        </DropdownToggle>
-                        <DropdownMenu right>
-                            <DropdownItem> FAQ </DropdownItem>
-                            <DropdownItem> Liability Waiver </DropdownItem>
-                        </DropdownMenu>
-                    </UncontrolledDropdown>
-                    <UncontrolledDropdown >
-                        <DropdownToggle nav caret>
-                            Player Portal
-                        </DropdownToggle>
-                        <DropdownMenu right>
-                            <DropdownItem> Log In </DropdownItem>
-                            <DropdownItem> How To </DropdownItem>
-                            <DropdownItem> Schedules </DropdownItem>
-                            <DropdownItem> Rule Books </DropdownItem>
-                            <DropdownItem> Roster </DropdownItem>
-                            <DropdownItem> League Standings </DropdownItem>
-                            <DropdownItem> League Leader </DropdownItem>
-                            <DropdownItem> How Can We Improve? </DropdownItem>
-                        </DropdownMenu>
-                    </UncontrolledDropdown>
-                    <StyledLink to="/">Social Events</StyledLink>
-                    <StyledLink to="/">Veterans</StyledLink>                    
-                    <StyledLink to="/">Corporate Sponsors</StyledLink>     
-                </Nav>
+            <StyledNavBar expand="md">
+                <NavHeader>
+                    <StyledLink to="/">NH Urban Sport</StyledLink>
+                </NavHeader>
+                <UncontrolledDropdown isOpen={isOpen1} onMouseEnter={() => setIsOpen1(true)} onMouseLeave={() => setIsOpen1(false)}>
+                    <StyledDropdownToggle nav>
+                        <StyledLink to="/">NH Urban Sport</StyledLink>
+                    </StyledDropdownToggle>
+                    <DropdownMenu>
+                        <DropdownItem>
+                            <StyledLink to="/about">About</StyledLink>
+                        </DropdownItem>
+                        <DropdownItem> 
+                            <StyledLink to="/">Contact</StyledLink>
+                        </DropdownItem>
+                    </DropdownMenu>
+                </UncontrolledDropdown>
+                <UncontrolledDropdown isOpen={isOpen2} onMouseEnter={() => setIsOpen2(true)} onMouseLeave={() => setIsOpen2(false)}>
+                    <StyledDropdownToggle nav>
+                        <StyledLink to="/about">Sports Leagues</StyledLink>
+                    </StyledDropdownToggle>
+                    <DropdownMenu>
+                        <DropdownItem><StyledLink to="/">FAQ</StyledLink></DropdownItem>
+                        <DropdownItem><StyledLink to="/">Liability Waiver</StyledLink></DropdownItem>
+                    </DropdownMenu>
+                </UncontrolledDropdown>
+                <UncontrolledDropdown isOpen={isOpen3} onMouseEnter={() => setIsOpen3(true)} onMouseLeave={() => setIsOpen3(false)}>
+                    <StyledDropdownToggle nav>
+                        <StyledLink to="/about">Player Portal</StyledLink>
+                    </StyledDropdownToggle>
+                    <DropdownMenu>
+                        <DropdownItem><StyledLink to="/">Log In</StyledLink></DropdownItem>
+                        <DropdownItem><StyledLink to="/">How To</StyledLink></DropdownItem>
+                        <DropdownItem><StyledLink to="/">Schedules</StyledLink></DropdownItem>
+                        <DropdownItem><StyledLink to="/">Rule Books</StyledLink></DropdownItem>
+                        <DropdownItem><StyledLink to="/">Roster</StyledLink></DropdownItem>
+                        <DropdownItem><StyledLink to="/">League Standings</StyledLink></DropdownItem>
+                        <DropdownItem><StyledLink to="/">League Leader</StyledLink></DropdownItem>
+                        <DropdownItem><StyledLink to="/">How Can We Improve?</StyledLink></DropdownItem>
+                    </DropdownMenu>
+                </UncontrolledDropdown>
+                <StyledLink to="/">Social Events</StyledLink>
+                <StyledLink to="/">Veterans</StyledLink>  
+                <StyledLink to="/">Corporate Sponsors</StyledLink>
             </StyledNavBar>
             <Switch>
                 <Route exact path = "/" component={Home} />
                 <Route path = "/about" component={About} />
+                <Route path = "/*" component={Home} />
             </Switch>
         </Router>
     );
