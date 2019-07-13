b0ab723ed873de3f23cedc1bc8070cc7ea8f3628
Faith Cuneo <userfriendly01@gmail.com>
2019-07-12 23:08:45 -0400

    At this point we can return objects instead of promises but I still can't enter the object

--- src/pages/Roster.jsx
+++ src/pages/Roster.jsx
@@ -3,42 +3,17 @@ import Container from '../components/Container.jsx'
 import Accordion from '../components/Accordian.jsx'
 import { getSports, getSport } from '../service/Sport.jsx'
 import { getAllTeamsForSport } from '../service/Team.jsx'
-import { getActiveSessions, getSessions } from '../service/Session.jsx'
+import { returnSessionValues } from '../service/Session.jsx'
 
   const Roster = () => {
         
     const [ sports, setSports ] = useState([]);
-    const [ activeSessions, setActiveSessions ] = useState([]);
+    const activeSessionFriendlyNames = returnSessionValues();
     const [ teams, setTeams ] = useState({});
     const [ players, setPlayers ] = useState(["Player 1", "Player 2", "Player 3"]);
     
-    const filterSessions = () => {
-      //update this to be an object of the Session Start Date and Sport Name
-      getActiveSessions().then((sessions) => {
-        let sessionsArray = [];
-        for(var s in sessions){
-          //get the startDate and Sport Id => date for each session
-          let startDate = sessions[s].startDate;
-          let sportFriendlyName;
-          getSport(sessions[s].sportKey).then((sport) => {
-            for(var x in sport){
-              sportFriendlyName = sport[x].sportName
-            }
-            sessionsArray.push(sportFriendlyName + " " + startDate)
-          })
-        }
-        setActiveSessions(sessionsArray);
-      })
-      
-      getSports().then((sports) => {
-        let sportNames = [];
-        for(var s in sports){
-          sportNames.push(sports[s].sportName);
-        }
-        setSports(sportNames);
-        console.log(sports)
-      });
-    }
+    console.log(returnSessionValues())
+    
 
     const filterTeams = () => {
       //For each sport, find the listed team names and add the sport:teams array to the overall teams object
@@ -59,7 +34,7 @@ import { getActiveSessions, getSessions } from '../service/Session.jsx'
 
 
     useEffect(() => {
-      filterSessions();
+      //filterSessions();
       filterTeams();
       filterPlayers();
     }, []);
@@ -69,7 +44,7 @@ import { getActiveSessions, getSessions } from '../service/Session.jsx'
             <h1>Roster!</h1>
             <dl className="accordion">
               {
-                activeSessions.map((item, index) => (
+                sports.map((item, index) => (
                   <div key={`sport${index}`}>
                     <Accordion 
                       title={item} 
--- src/service/Session.jsx
+++ src/service/Session.jsx
@@ -1,18 +1,45 @@
 import { getDatabase } from '../util/Connect.jsx'
+import { getSport } from './Sport.jsx'
 
+//Establish database connection pointed to Sessions
 const myDatabase = getDatabase();
 const sessionRef = myDatabase.ref("Sessions");
+let returnValues = {};
+var test;
+let sessions = "wasssup";
 
-export const getSessions = () => {
+//Get Return Values
+
+export const returnSessionValues = () => {
+    console.log(returnValues)
+    setShit();
+    console.log(returnValues.activeSessionSnapshot)
+    return returnValues;
+}
+
+const setShit = () => {
+    clean().then((resolved) => {
+        console.log(returnValues);
+    })
+}
+
+//Basic CRUD methods
+
+export const getSessions = () => {    
     return sessionRef
             .once('value')
             .then((snapshot) => {
-                return snapshot.val();
+                console.log(sessions)
+                sessions = snapshot.val()
+                console.log(sessions)
+            }).then((finalReturn) => {
+                return finalReturn
             })
 }
 
+//Custom business logic
+
 export const getActiveSessions = () => {
-    //Can I say only return sessions where active is true
     return sessionRef
             .orderByChild('active')
             .equalTo(true)
@@ -21,3 +48,32 @@ export const getActiveSessions = () => {
                 return snapshot.val();
             })
 }
+
+const filterSessions = () => {
+     return getActiveSessions().then((sessions) => {
+      let sessionsArray = [];
+      for(var s in sessions){
+        let startDate = sessions[s].startDate;
+        let sportFriendlyName;
+        return getSport(sessions[s].sportKey).then((sport) => {
+          for(var x in sport){
+            sportFriendlyName = sport[x].sportName
+          }
+          sessionsArray.push(sportFriendlyName + " " + startDate)
+        })
+      }
+      returnValues.activeSessionFriendlyNames = sessionsArray;
+    })
+  }
+
+  const clean = () => {
+      //get active session
+      return getActiveSessions().then((activeSessionSnapshot) => {
+          console.log(activeSessionSnapshot)
+        returnValues.activeSessionSnapshot = activeSessionSnapshot;
+        return activeSessionSnapshot;
+      })
+        //get the start Date
+        //get the sportId
+
+  }
