4433eb0e9b0a324ec05e82728b02319dc884906c
Faith Cuneo <userfriendly01@gmail.com>
2020-03-06 11:44:54 -0500

    updated liability waiver

--- src/pages/LiabilityWaiver/LiabilityWaiver.jsx
+++ src/pages/LiabilityWaiver/LiabilityWaiver.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import { Container } from '../../components'
+import {
+    DisplayCard, 
+    TextCard,
+    Wrapper 
+} from '../../components'
 
 const LiabilityWaiver = () => {
     return(
-        <Container margin="20">
-            <iframe src="https://docs.google.com/document/d/e/2PACX-1vQkfPJFZovcEt__gjqKzBKtPtIDBb6-kyQ2ga1FcMvcZdBKiDNcYoJz6MIBPXPxa5vDAl47gTySjn_v/pub?embedded=true"
-                        scrolling="yes"
-                        height="1000px"
-                        width="800px">
-            </iframe>
-        </Container>
+        <Wrapper margin="20" width="750">
+          <DisplayCard>
+            <TextCard id={"Liability Waiver"} />
+          </DisplayCard>
+        </Wrapper>
     )
 }
 
